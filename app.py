dd69208bb7ecf7141508858d08752153efecc9f8
MOHITH172005 <valasareddymohith17@gmail.com>
2026-07-25 14:15:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,54 +1,67 @@
-
-from flask import Flask,request, url_for, redirect, render_template
+from flask import Flask, request, render_template
 import joblib
 import numpy as np
+import os
 
 app = Flask(__name__)
 
-test = pickle.load(open('test1.pkl','rb'))
+# Load trained model
+MODEL_PATH = os.path.join(os.path.dirname(__file__), "test1.pkl")
+test = joblib.load(MODEL_PATH)
 
 
-@app.route('/')
+@app.route("/")
 def hello_world():
     return render_template("t.html")
 
-@app.route('/predict',methods=['POST','GET'])
+
+@app.route("/predict", methods=["POST", "GET"])
 def predict():
-    int_features=[int(x) for x in request.form.values()]
-    final=[np.array(int_features)]
-    print(int_features)
-    print(final)
-    prediction=test.predict(final)
+    if request.method == "POST":
+        int_features = [int(x) for x in request.form.values()]
+        final = np.array([int_features])
 
-    if prediction == 0:
-        return render_template('t.html',pred="\t\t\t\t\tProbability of accident severity is : Minor")
-    else:
-        return render_template('t.html',pred="\t\t\t\t\tProbability of accident severity is : Major")
+        prediction = test.predict(final)
 
-@app.route('/Map')
+        if prediction[0] == 0:
+            result = "Probability of accident severity is : Minor"
+        else:
+            result = "Probability of accident severity is : Major"
+
+        return render_template("t.html", pred=result)
+
+    return render_template("t.html")
+
+
+@app.route("/Map")
 def map1():
-    return render_template("map.html")    
+    return render_template("map.html")
 
-@app.route('/Graphs')
+
+@app.route("/Graphs")
 def graph():
     return render_template("graph.html")
 
-@app.route('/Map1')
+
+@app.route("/Map1")
 def map2():
     return render_template("ur.html")
 
-@app.route('/Map2')
+
+@app.route("/Map2")
 def map3():
     return render_template("bs.html")
 
-@app.route('/Map3')
+
+@app.route("/Map3")
 def map4():
     return render_template("hm.html")
 
-@app.route('/Pie')
+
+@app.route("/Pie")
 def pie():
     return render_template("pie.html")
 
 
-if __name__=="__main__":
-    app.run() 
+if __name__ == "__main__":
+    app.run(debug=True)
